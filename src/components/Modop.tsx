3dabf13c7e302f56228e1f01a856ed1abac4d784
Gokul75rvn <gokulofficial75rvn@gmail.com>
2025-08-03 17:30:39 +0530

    perfect

--- src/components/Modop.tsx
+++ src/components/Modop.tsx
@@ -1,5 +1,5 @@
 
-import { Input } from '@/components/ui/input'
+import { Input } from './ui/input'
 import { Button } from "@/components/ui/button";
 
 import { Textarea } from '@/components/ui/textarea';
